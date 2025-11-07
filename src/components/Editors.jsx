5f914e070b0a2ac845c311c0b007b85835d59857
sinay135 <sinay135@mymail.unisa.edu.au>
2025-11-08 03:35:57 +1030

    side by side editors

--- src/components/Editors.jsx
+++ src/components/Editors.jsx
@@ -3,12 +3,20 @@
 
 export default function Editors({defaultValue, onChange}) {
     return (
-        <div className="row">
-            <div className="col-11">
-                <div className="ps-1" style={{ maxHeight: '58vh', overflowY: 'auto'}}>
-                    <textarea className="rounded-0 form-control" defaultValue={defaultValue} onChange={onChange} style={{borderTopRightRadius: "1", scrollbarWidth: "thin", scrollbarColor: 'grey rgba(230, 230, 230, 1)'}} rows="8" id="proc" ></textarea>
+        <div className="row g-0">
+            <div className="col-5">
+                <div className="ps-1" style={{overflowY: 'hidden', maxHeight: '90vh'}}>
+                    <textarea   className="rounded-0 form-control" 
+                                defaultValue={defaultValue} 
+                                onChange={onChange} 
+                                style={{borderTopRightRadius: "1", scrollbarWidth: "thin", scrollbarColor: 'lightgreen rgba(30, 30, 30, 1)', backgroundColor: "lightgreen"}} 
+                                rows="27" 
+                                id="proc" >
+                    </textarea>
                 </div>
-                <div className="ps-1 pe-0" style={{ maxHeight: '58vh', overflowY: 'auto', scrollbarWidth: "thin", scrollbarColor: 'lightgreen rgba(30, 30, 30, 1)'}}>
+            </div>
+            <div className="col-6">
+                <div className="" style={{ maxHeight: '90vh', overflowY: 'auto', scrollbarWidth: "thin", scrollbarColor: 'lightgreen rgba(30, 30, 30, 1)'}}>
                     <div id="editor" />
                     <div id="output" />
                 </div>
